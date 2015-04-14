3bed31b0401fd66b4bde315ab4efc3644790f974
Andy Joiner <andy.joiner@tekkies.co.uk>
2015-04-14 20:21:12 +0100

    risk1:experiment:analyse:First attempt at FFT.

---
+++ ListenLearn.Listen/Core/AforgeFftAnalyser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Math;
+
+
+namespace ListenLearn.Listen.Core
+{
+    public class AforgeFftAnalyser : Analyser
+    {
+        public Complex[] Analyse(Double[] input)
+        {
+            var complexArray = ToComplex(input);
+            FourierTransform.FFT(complexArray, FourierTransform.Direction.Forward);
+            return complexArray;
+        }
+
+        private Complex[] ToComplex(double[] input)
+        {
+            var complex = new Complex[input.Length];
+            for(int i=0;i<input.Length;i++)
+            {
+                complex[i] = new Complex(input[i], 0);
+            }
+            return complex;
+        }
+    }
+}
--- ListenLearn.ListenTest/AnalyserTest.cs
+++ ListenLearn.ListenTest/AnalyserTest.cs
@@ -1,4 +1,8 @@
-﻿using ListenLearn.Listen.Core;
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using AForge.Math;
+using ListenLearn.Listen.Core;
 using NUnit.Framework;
 
 namespace ListenLearnTest
@@ -9,7 +13,28 @@ namespace ListenLearnTest
         [Test]
         public void AnalyserTest_CreateAnalyser()
         {
-            var anlyser = new AforgeAnalyser();
+            var analyser = new AforgeFftAnalyser();
+            double[] sin = RenderSin(0.5, 0.5, 512);
+            var output = analyser.Analyse(sin);
+            Print(output);
+        }
+
+        private double[] RenderSin(double frequency, double amplitude, int samples)
+        {
+            var output = new double[samples];
+            for (int i = 0; i < samples; i++)
+            {
+                output[i] = amplitude*Math.Sin(frequency*i);
+            }
+            return output;
+        }
+
+        private void Print(Complex[] output)
+        {
+            foreach (var item in output)
+            {
+                Console.WriteLine(Math.Abs(item.Re)); 
+            }
         }
     }
 }
